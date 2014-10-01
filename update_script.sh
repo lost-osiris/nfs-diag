7db0e6c2e757ed920958b2dfcea7626dea7519fd
lost-osiris <farva74@gmail.com>
2014-10-01 05:15:56 -0400

    updating update process

--- update_script.sh
+++ update_script.sh
@@ -12,7 +12,7 @@ if [[ "$(echo -e $remote_status | grep "behind")" != "" ]]; then
    git --git-dir=$1 fetch --all --force
    git --git-dir=$1 reset --hard
    git --git-dir=$1 rebase origin/master
-   cp $(echo -e $1)/repo/* $(echo -e $1)current/
+   cp $(echo -e $1)repo/* $(echo -e $1)current/
 else
    echo -e "Already up-to-date"
 fi   
