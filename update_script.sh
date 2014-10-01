5876b319b10bb3590ea12cdc1f590a70c505ba5a
lost-osiris <farva74@gmail.com>
2014-10-01 05:22:37 -0400

    updating update process

--- update_script.sh
+++ update_script.sh
@@ -6,7 +6,7 @@ rm -f $(echo -e $1)current/*.pyo
 rm -f $(echo -e $1)current/*.pyc
 rm -f $(echo -e $1)repo/*.pyo
 rm -f $(echo -e $1)repo/*.pyc
-
+echo "blah"
 if [[ "$(echo -e $remote_status | grep "behind")" != "" ]]; then
    git --git-dir=$1 fetch --all --force
    git --git-dir=$1 reset --hard
