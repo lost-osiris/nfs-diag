6f7b09f227e84a1b5ed45ce78190cd2a8e4c9211
lost-osiris <farva74@gmail.com>
2014-10-01 05:25:56 -0400

    updating update process

--- update_script.sh
+++ update_script.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
-remote=$(git --git-dir=$1 remote update &> /dev/null)
-remote_status=$(git --git-dir=$1 status -uno)
+repo="$(echo -e $1)repo/.git"
+remote=$(git --git-dir=$repo remote update &> /dev/null)
+remote_status=$(git --git-dir=$repo status -uno)
 rm -f $(echo -e $1)current/*.pyo
 rm -f $(echo -e $1)current/*.pyc
 rm -f $(echo -e $1)repo/*.pyo
 rm -f $(echo -e $1)repo/*.pyc
-echo "blah"
 if [[ "$(echo -e $remote_status | grep "behind")" != "" ]]; then
-   git --git-dir=$1 fetch --all --force
-   git --git-dir=$1 reset --hard
-   git --git-dir=$1 rebase origin/master
+   git --git-dir=$repo fetch --all --force
+   git --git-dir=$repo reset --hard
+   git --git-dir=$repo rebase origin/master
    src="$(echo -e $1)repo/*"
    dest="$(echo -e $1)current/"
    cp $src $dest
