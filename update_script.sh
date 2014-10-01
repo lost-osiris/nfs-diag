46b434ff177055e6bb6b728316e1938fce1a1b11
lost-osiris <farva74@gmail.com>
2014-10-01 06:04:20 -0400

    updating update process

--- update_script.sh
+++ update_script.sh
@@ -6,17 +6,18 @@ rm -f $(echo -e $1)current/*.pyo
 rm -f $(echo -e $1)current/*.pyc
 rm -f $(echo -e $1)repo/*.pyo
 rm -f $(echo -e $1)repo/*.pyc
-echo "blah"
+
 if [[ "$(echo -e $remote_status | grep "behind")" != "" ]]; then
    rm -f $(echo -e $1)repo/*
    rm -rf $(echo -e $1)repo/.git
-   git clone https://github.com/lost-osiris/nfs-diag.git $1/repo
+   git clone https://github.com/lost-osiris/nfs-diag.git $1/repo &
    #git --git-dir=$repo fetch --all --force
    #git --git-dir=$repo reset --hard
    #git --git-dir=$repo rebase origin/master
    src="$(echo -e $1)repo/*"
    dest="$(echo -e $1)current/"
    cp $src $dest
+   echo -e "*** Successfully Updated ***"
 else
    echo -e "Already up-to-date"
 fi   
