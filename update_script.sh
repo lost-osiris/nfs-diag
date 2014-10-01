bb0b7589baa7431513df6a09d086b0e4044e5e89
lost-osiris <farva74@gmail.com>
2014-10-01 05:11:31 -0400

    updating update process

--- update_script.sh
+++ update_script.sh
@@ -3,14 +3,15 @@
 remote=$(git --git-dir=$1 remote update)
 remote_status=$(git --git-dir=$1 status -uno)
 
+rm -f $(echo-e $1)current/*.pyo
+rm -f $(echo -e $1)current/*.pyc
+rm -f $(echo -e $1)repo/*.pyo
+rm -f $(echo -e $1)repo/*.pyc
+
 if [[ "$(echo -e $remote_status | grep "behind")" != "" ]]; then
    git --git-dir=$1 fetch --all --force
    git --git-dir=$1 reset --hard
    git --git-dir=$1 rebase origin/master
-   rm -f $(echo-e $1)current/*.pyo
-   rm -f $(echo -e $1)current/*.pyc
-   rm -f $(echo -e $1)repo/*.pyo
-   rm -f $(echo -e $1)repo/*.pyc
    cp $(echo -e $1)/repo/* $(echo -e $1)current/
 else
    echo -e "Already up-to-date"
