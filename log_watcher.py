b54691bba7360d73b566d8529292fab7653a3309
lost-osiris <farva74@gmail.com>
2014-10-01 14:15:58 -0400

    -C command fix

--- log_watcher.py
+++ log_watcher.py
@@ -10,7 +10,6 @@ class LogWatcher:
       self.messages = messages
       self.pids = pids
       self.obj = obj
-      print self.messages
       pool = []
       for i in self.location:
          j = multiprocessing.Process(target=self.watch, args=(i,))
@@ -43,13 +42,6 @@ class LogWatcher:
                current_time = time.strftime("%H:%M")
                current_date = time.strftime("%b %d")
                for i in self.messages:
-                  self.obj.logger.log(i)
-                  self.obj.logger.log(line)
-                  print str(i) in str(line)
-                  print current_time
-                  print str(current_time) in str(line)
-                  print current_date
-                  print str(current_date) in str(line)
                   
                   if i in line and current_time in line and current_date in line:
                      self.obj.logger.log(str("\nFound message \"" + i + "\" in " + location + "\nKilling tcpdump"))
